f98e6c001fbda9c6f48a7e5975960dcb4decbf3a
nithinskumar866 <nithinsofficial86@gmail.com>
2025-03-18 14:46:10 +0530

    code.java

---
+++ soluyion.java
@@ -0,0 +1,14 @@
+public class soluyion {
+
+    public static void main(String[] args) {
+        
+        int rem;
+        long rev=0;
+        while(x!=0)
+        {
+            rem=x%10;
+            rev=rev*10
+        }
+
+    }
+}
