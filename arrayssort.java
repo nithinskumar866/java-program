43c9e6f1b8f496fcab4d4bbed16e29679cf3ec0c
nithinskumar866 <nithinsofficial86@gmail.com>
2025-03-18 10:44:23 +0530

    Arrayssort

--- arrayssort.java
+++ arrayssort.java
@@ -1,6 +1,28 @@
 import java.util.*;
 public class arrayssort {
     public static void main(String[] args) {
-        
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter");
+        int sz=sc.nextInt();
+        int arr[]=new int[sz];
+        for(int i=0;i<sz;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        int temp=0;
+        for(int i=0;i<sz;i++)
+        {
+            for(int j=0;j<sz;j++)
+            {
+                if(arr[i]>arr[j])
+                {
+                    
+                    temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
